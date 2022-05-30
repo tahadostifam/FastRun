c74a899e272ec7d2786e2bdf7712b093bcbd9ffb
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-05-30 13:19:55 -0400

    install.sh almost finished! moving to version 1.2

--- install.sh
+++ install.sh
@@ -1,13 +1,43 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env /bin/bash
 
-cd /tmp
+function install(){
+    cd /tmp
 
-git clone https://github.com/tahadostifam/FastRun.git
+    git clone https://github.com/tahadostifam/FastRun.git
 
-cd FastRun
+    cd FastRun
 
-bundle
+    bundle
 
-sudo chmod +x ./fastrun
+    sudo chmod +x ./fastrun
 
-LATEST_VERSION=$()
+    sudo cp ./fastrun /usr/local/bin
+}
+
+function get_latest_version(){
+    latest_version=$(curl --silent https://raw.githubusercontent.com/tahadostifam/FastRun/main/latest_version)
+
+    echo $latest_version
+}
+
+function get_local_installed_version(){
+    version=$(/usr/local/bin/fastrun -v)
+    echo $(trim_string ${version})
+}
+
+function trim_string(){
+    echo "$1" | sed 's/ *$//g'
+}
+
+if [[ -f "/usr/local/bin/fastrun" ]]; then
+    local_version=$(get_local_installed_version | bc)
+    latest_version=$(get_latest_version | bc)
+    
+    if [ $local_version \< $latest_version ]; then
+        install
+    else
+        echo "Latest version of fastrun installed on your system."
+    fi;
+else
+    install
+fi;
